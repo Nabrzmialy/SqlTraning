88c3be157baf346088535fe4be33d3efbca4ef6a
ECODEV\mkoziol <mkoziol@ecovadis.com>
2020-08-29 20:23:17 +0200

    fix16

---
+++ 4 - Joins/4.10_Solution.sql
@@ -0,0 +1,16 @@
+﻿SELECT 
+	soh.SalesPersonID as [Sprzedawca],
+	soh.TotalDue as [Kwota],
+	T.* 
+FROM [Sales].SalesOrderHeader soh 
+INNER JOIN
+(
+	SELECT 
+		sod.SalesOrderID,
+		COUNT(sod.SalesOrderID) as [Liczba Pozycji]
+	FROM [Sales].SalesOrderHeader soh
+	INNER JOIN [Sales].SalesOrderDetail sod on soh.SalesOrderID = sod.SalesOrderID
+	GROUP BY sod.SalesOrderID
+	HAVING (COUNT(sod.SalesOrderID) > 10)
+) T ON soh.SalesOrderID = T.SalesOrderID
+order by salesorderId
