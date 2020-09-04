43fef880ff431910e47165c323573bdfa23cc5d3
ECODEV\mkoziol <mkoziol@ecovadis.com>
2020-09-04 14:27:27 +0200

    xxxx12

---
+++ 4 - Joins/4.17_Exercise.sql
@@ -0,0 +1,17 @@
+﻿/* 
+	
+	UPDATE ShipDate (+1 DAY) for all SalesOrderHeader
+	
+	WHERE the ship method name = 'CARGO TRANSPORT 5' 
+
+*/
+
+SELECT DATEADD(day, 1, GETDATE())
+SELECT DATEADD(month, 1, GETDATE())
+SELECT DATEADD(year, 1, GETDATE())
+
+BEGIN TRAN
+
+ROLLBACK TRAN
+
+-- 3806 rows should be affected
---
+++ 4 - Joins/4.17_Present.sql
@@ -0,0 +1,62 @@
+﻿-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- 
+-- WHAT IS TRANSACTION
+
+BEGIN TRANSACTION
+
+SELECT * FROM Person.Person WHERE BusinessEntityID = 69
+
+UPDATE Person.Person
+SET MiddleName = 'Donald'
+WHERE BusinessEntityID = 69
+
+SELECT * FROM Person.Person WHERE BusinessEntityID = 69
+
+COMMIT TRANSACTION
+
+----------------------------------------------------------------------------------------------------------------------------------
+-- What if we do not want to apply our changes
+
+BEGIN TRANSACTION
+
+SELECT * FROM Person.Person WHERE BusinessEntityID = 69
+
+UPDATE Person.Person
+SET MiddleName = 'Lalalala'
+WHERE BusinessEntityID = 69
+
+SELECT * FROM Person.Person WHERE BusinessEntityID = 69
+
+ROLLBACK TRANSACTION
+
+SELECT * FROM Person.Person WHERE BusinessEntityID = 69
+
+----------------------------------------------------------------------------------------------------------------------------------
+-- Time for interesting update... with join
+-- Let's update People Middle Name to type of phone  :-)
+
+SELECT * FROM Person.PhoneNumberType
+
+-- Let's take a look at our data
+SELECT 
+	p.BusinessEntityID,
+	pp.PhoneNumber,
+	pnt.[Name]
+FROM Person.Person p
+inner join Person.PersonPhone pp on p.BusinessEntityID = pp.BusinessEntityID
+inner join Person.PhoneNumberType pnt on pp.PhoneNumberTypeID = pnt.PhoneNumberTypeID
+
+-- update time
+BEGIN TRAN
+
+	UPDATE
+	    person
+	SET
+	    person.MiddleName = pnt.[Name]
+	FROM 
+	    Person.Person person
+	    inner join Person.PersonPhone pp on person.BusinessEntityID = pp.BusinessEntityID
+		inner join Person.PhoneNumberType pnt on pp.PhoneNumberTypeID = pnt.PhoneNumberTypeID
+	
+	SELECT * FROM Person.Person
+
+ROLLBACK TRAN
