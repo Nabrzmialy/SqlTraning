7406318adcfe1978b0a1d1bfbbf9f3c51824011e
ECODEV\mkoziol <mkoziol@ecovadis.com>
2020-09-04 14:38:34 +0200

    xxxx13

---
+++ 4 - Joins/4.17_Solution.sql
@@ -0,0 +1,12 @@
+﻿BEGIN TRAN
+	
+	UPDATE
+	    soh
+	SET
+	    soh.ShipDate = DATEADD(day, 1, soh.ShipDate)
+	FROM 
+	    Sales.SalesOrderHeader soh
+	    inner join Purchasing.ShipMethod sm on soh.ShipMethodID = sm.ShipMethodID
+		where sm.[Name] = 'CARGO TRANSPORT 5'
+
+ROLLBACK TRAN
---
+++ 4 - Joins/4.18_Exercise.sql
@@ -0,0 +1,15 @@
+﻿/* 
+	
+	UPDATE ListPrice of Production.Product add 8.69 USD
+	To products with category = 'Clothing'
+	and Color = 'Black'
+	
+*/
+
+
+
+BEGIN TRAN
+
+ROLLBACK TRAN
+
+-- 16 rows should be affected
