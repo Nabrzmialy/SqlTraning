e3e6fbb5e1e65e166761b345e4f90363a4f9cb48
ECODEV\mkoziol <mkoziol@ecovadis.com>
2020-09-04 10:50:05 +0200

    xxxx7

---
+++ 4 - Joins/4.13_Exercise.sql
@@ -0,0 +1,16 @@
+﻿/* 
+	Show 
+	a) all orders with more than 1 sales reasons + assigned to clients who has 0 phones numbers
+	b) show email of such customer
+
+	Tables to use: 
+	a) Sales.SalesOrderHeader
+	b) Sales.SalesOrderHeaderSalesReason
+	c) Sales.Customer
+	d) Person.Person
+	e) Person.EmailAddress
+	f) Person.PersonPhone
+
+*/
+
+EXEC [4.13].[Solution]
