068b9a7be2661cb8351550dfae428149676aaf79
ECODEV\mkoziol <mkoziol@ecovadis.com>
2020-09-04 11:48:34 +0200

    xxxx10

---
+++ 4 - Joins/4.11_Solution.sql
@@ -0,0 +1,10 @@
+﻿SELECT 
+	prod.ProductID [Id Produktu],
+	prod.[Name] as [Nazwa],
+	pc.[Name] as [Kategoria Produktu],
+	ps.[Name] as [Subkategoria Produktu]
+FROM [Production].Product prod 
+LEFT JOIN [Production].ProductSubcategory ps on prod.ProductSubcategoryID = ps.ProductSubcategoryID
+LEFT JOIN [Production].ProductCategory pc on ps.ProductCategoryID = pc.ProductCategoryID
+
+
