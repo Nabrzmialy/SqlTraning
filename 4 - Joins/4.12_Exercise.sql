ea0819c3b9dba7f44bad1dee3340697c577a9149
ECODEV\mkoziol <mkoziol@ecovadis.com>
2020-09-04 11:49:27 +0200

    xxxx11

---
+++ 4 - Joins/4.12_Exercise.sql
@@ -0,0 +1,11 @@
+﻿/* 
+	Show 
+	a) all orders with more than 1 sales reason + customer Id
+
+	Tables to use: 
+	a) Sales.SalesOrderHeader
+	b) Sales.SalesOrderHeaderSalesReason
+
+*/
+
+EXEC [4.12].[Solution]
