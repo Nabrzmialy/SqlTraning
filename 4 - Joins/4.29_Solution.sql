ed694324517dc1d739db32ef0fb4868dd074ebe5
ECODEV\mkoziol <mkoziol@ecovadis.com>
2020-09-11 13:00:53 +0200

    xx379

---
+++ 4 - Joins/4.29_Solution.sql
@@ -0,0 +1,26 @@
+﻿SELECT * FROM Sales.SalesOrderHeader soh 
+inner join (
+SELECT OrderDate, max(SalesOrderID) as [LastOrder] FROM Sales.SalesOrderHeader
+GROUP BY OrderDate
+) INN ON soh.SalesOrderID = inn.LastOrder and soh.OrderDate = inn.OrderDate
+
+---
+SELECT * FROM
+(
+	SELECT
+		Max(SalesOrderID) OVER(PARTITION BY OrderDate) as [LastOrder], 
+		*
+	FROM Sales.SalesOrderHeader
+) INN 
+WHERE INN.[LastOrder] = inn.SalesOrderID
+
+---
+SELECT * FROM
+(
+	SELECT
+		ROW_NUMBER() OVER(PARTITION BY OrderDate ORDER BY SalesOrderID DESC) AS 'Rank',
+		*
+	FROM Sales.SalesOrderHeader
+) INN 
+WHERE INN.[Rank] = 1
+
