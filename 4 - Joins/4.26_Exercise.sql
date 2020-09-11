035c34c869450c9e0260118eabdff12b48d55bc1
ECODEV\mkoziol <mkoziol@ecovadis.com>
2020-09-11 14:47:45 +0200

    xx37ggjo

---
+++ 4 - Joins/4.26_Exercise.sql
@@ -0,0 +1,7 @@
+﻿/*
+Find orders ids where sales person and customer person were:
+a) from same territory
+b) different teritory
+c) find customers from germany + show full name as
+d) achieve same query plan
+*/
---
+++ 4 - Joins/4.26_Present.sql
@@ -0,0 +1,51 @@
+﻿DROP TABLE IF EXISTS Sales 
+CREATE TABLE [dbo].[Sales](
+	[Region] [varchar](50) ,
+	[SumUp] [varchar](200) NULL,
+	[Country] [varchar](50) ,
+	[ItemType] [varchar](50) NULL,
+	[OrderPriority] [varchar](50) NULL,
+	[OrderDate]  datetime,
+	[OrderID] bigint NULL,
+)
+
+BULK INSERT Sales
+FROM 'C:\_GIT\SqlFan\8- Performance\1500000 Sales Records.csv'
+WITH (FIRSTROW = 2,
+	KEEPNULLS,
+    FIELDTERMINATOR = ',',
+	ROWTERMINATOR='\n' );
+
+
+
+SELECT TOP 10000 * 
+INTO #SalesTemporary
+FROM Sales
+
+SELECT * FROM #SalesTemporary
+
+SELECT TOP 10000 * 
+INTO ##SalesTemporary
+FROM Sales
+
+-- Show names of generated tables.
+--------------------------------------------------------------------------------------------
+DROP TABLE #SalesTemporary
+DROP TABLE ##SalesTemporary
+
+
+CREATE TABLE #SalesTemporary(
+	[Region] [varchar](50) ,
+	[SumUp] [varchar](200) NULL,
+	[Country] [varchar](50) ,
+	[ItemType] [varchar](50) NULL,
+	[OrderPriority] [varchar](50) NULL,
+	[OrderDate]  datetime,
+	[OrderID] bigint NULL,
+)
+
+INSERT INTO #SalesTemporary
+SELECT TOP 10000 * FROM Sales
+
+SELECT * FROM #SalesTemporary
+-- check accessibility of global and local temp tables
