fcf4f06cf6c62204c8a239c865e3e8e3b6e29131
ECODEV\mkoziol <mkoziol@ecovadis.com>
2020-09-11 10:48:55 +0200

    xx34

---
+++ 4 - Joins/4.28_Exercise.sql
@@ -0,0 +1,5 @@
+﻿-- EXERCISE  --
+/*
+Start transaction scope in program and execute update and catch breakpoint. 
+Find process using sp_who2 and kill the process.
+*/
---
+++ 4 - Joins/4.28_Present.sql
@@ -0,0 +1,8 @@
+﻿SELECT * FROM SALES where OrderID= 120712983
+
+BEGIN TRAN
+	UPDATE Sales set OrderDate = GETDATE() WHERE OrderID = 120712983
+
+
+exec sp_who2 
+kill 57   
