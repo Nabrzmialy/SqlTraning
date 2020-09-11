8893dc7869242e2cb626752e18325f343276855d
ECODEV\mkoziol <mkoziol@ecovadis.com>
2020-09-11 11:34:14 +0200

    xx37

---
+++ 4 - Joins/4.22_Exercise_2ndChance.sql
@@ -0,0 +1,14 @@
+﻿DROP TABLE IF EXISTS Sales 
+CREATE TABLE [dbo].[Sales](
+	[Region] [varchar](50) ,
+	[SumUp] [varchar](200) NULL,
+	[Country] [varchar](50) ,
+	[ItemType] [varchar](50) NULL,
+	[OrderPriority] [varchar](50) NULL,
+	[OrderDate]  datetime,
+	[OrderID] bigint NULL,
+	[RandomInt] int null,
+	[NewName] [varchar](50) null,
+)
+
+-- insert bulk file not touching table definition
