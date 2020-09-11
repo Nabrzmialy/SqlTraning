3d300b400289869aa0d5060e62e7a041b926198b
ECODEV\mkoziol <mkoziol@ecovadis.com>
2020-09-11 12:35:22 +0200

    xx37

---
+++ 4 - Joins/4.29_Exercise.sql
@@ -0,0 +1,4 @@
+﻿/*
+Find last order from each day using 3 ways - compare performance.
+TABLE: Sales.SalesOrderHeader
+*/
---
+++ 4 - Joins/4.29_Present.sql
@@ -0,0 +1,34 @@
+﻿SET STATISTICS IO ON
+SET STATISTICS TIME ON
+
+--check performance
+SELECT INN.MaxLineTotal, sod.* FROM Sales.SalesOrderDetail sod
+inner join
+(
+	SELECT Max(LineTotal) as [MaxLineTotal], SalesOrderID
+	FROM Sales.SalesOrderDetail
+	GROUP BY SalesOrderID
+) INN on inn.MaxLineTotal = sod.LineTotal and inn.SalesOrderID = sod.SalesOrderID
+
+--check performance
+SELECT * FROM
+(
+	SELECT
+		Max(LineTotal) OVER(PARTITION BY SalesOrderID) as [MaxLineTotal], 
+		*
+	FROM Sales.SalesOrderDetail
+) INN 
+WHERE INN.MaxLineTotal = inn.LineTotal
+
+
+-- check perfomrance
+SELECT * FROM
+(
+	SELECT
+		ROW_NUMBER() OVER(PARTITION BY SalesOrderID ORDER BY LineTotal DESC) AS 'Rank',
+		*
+	FROM Sales.SalesOrderDetail
+) INN 
+WHERE INN.[Rank] = 1
+
+
