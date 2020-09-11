1e8c05c817a35c73129c27d8e88c22c1a6f7347f
ECODEV\mkoziol <mkoziol@ecovadis.com>
2020-09-11 15:20:53 +0200

    xx37ggjop

---
+++ 4 - Joins/4.27_Exercise.sql
@@ -0,0 +1,31 @@
+﻿-- EXERCISE 1 --
+/*
+Compare performance of update 10, 100, 100, 1000, 2500, 5000, 10000, all elements 
+compared to normal update.
+*/
+
+-- EXERCISE 2 --
+/* 
+Caluculate sum of 20000 prices of certain products + show receipt.
+Snacks
+Fruits 
+Cereal
+Clothes.
+Ignore other types of product. Use table Sales.
+
+Algorithm:
+Snacks 4329429384
+(6) = (432942)9384
+Price of snacks is "432942".
+Add to receipt "Snacks - 432942 \n"
+*/
+
+-- EXERCISE 3 --
+/*
+Show matrix using cursors via print.
+
+1 1 1 ... (100)
+1 1 1
+1 1 1
+(100)
+*/
---
+++ 4 - Joins/4.27_Present.sql
@@ -0,0 +1,34 @@
+﻿SET NOCOUNT ON; -- show what's that
+
+DECLARE @Start datetime2 = CURRENT_TIMESTAMP  
+
+DECLARE @SalesOrderID INT
+DECLARE Sales_Cursor CURSOR FOR
+	select TOP 10000 
+		SalesOrderID
+	from 
+		sales.SalesOrderHeader
+
+OPEN Sales_Cursor;
+FETCH NEXT FROM Sales_Cursor 
+INTO @SalesOrderID
+
+WHILE @@FETCH_STATUS = 0
+    BEGIN
+        
+		PRINT @SalesOrderID
+		
+		UPDATE sales.SalesOrderHeader SET SubTotal = SubTotal
+		WHERE SalesOrderID = @SalesOrderID
+
+		FETCH NEXT FROM Sales_Cursor INTO 
+		    @SalesOrderID;
+    END;
+CLOSE Sales_Cursor;
+DEALLOCATE Sales_Cursor;
+
+DECLARE @end datetime2 = CURRENT_TIMESTAMP  
+SELECT DATEDIFF(MILLISECOND, @Start, @end) as [Diff]
+
+
+
