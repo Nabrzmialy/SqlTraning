ac6c267da1a1f3c9cbcefe659e68761666240e77
ECODEV\mkoziol <mkoziol@ecovadis.com>
2020-09-25 11:24:36 +0200

    xxxx109

---
+++ 4 - Joins/4.34_Exercise.sql
@@ -0,0 +1,29 @@
+﻿	SELECT 
+		prod.ProductID [Id Produktu],
+		prod.[Name] as [Nazwa],
+		pc.[Name] as [Kategoria Produktu],
+		ps.[Name] as [Subkategoria Produktu]
+	FROM [Production].Product prod 
+	LEFT JOIN [Production].ProductSubcategory ps on prod.ProductSubcategoryID = ps.ProductSubcategoryID
+	LEFT JOIN [Production].ProductCategory pc on ps.ProductCategoryID = pc.ProductCategoryID
+
+-- Craete stored procedure to filter this query by LIKE '%Bike%'
+-- ProductName
+-- CategoryName
+-- SubCategoryName
+-- Product ID
+
+CREATE PROCEDURE uspGetProductsWithCategories
+
+-- Add parameters for paging
+--@PageNumber
+--@PageSize
+
+
+--- Filter by products IDs - modify procedure to use int list - by creating own type
+
+CREATE TYPE [dbo].[GuidList] AS TABLE(
+	[Item] [uniqueidentifier] NULL
+)
+
+-- 
---
+++ 4 - Joins/4.34_Present.sql
@@ -0,0 +1,89 @@
+﻿CREATE TABLE Employee (EmpID int identity(1,1),EmpName varchar(500))
+
+CREATE PROCEDURE uspEmployeeInsert
+(@EmpName varchar(50),
+@EId int output)
+AS
+BEGIN
+SET NOCOUNT ON
+ 
+INSERT INTO Employee (EmpName) VALUES (@EmpName)
+ 
+SELECT @EId= SCOPE_IDENTITY()
+ 
+END
+
+DECLARE @ReturnInt INT = 0
+exec uspEmployeeInsert 'ExemplaryEmployee', @ReturnInt OUTPUT
+SELECT @ReturnInt
+SELECT * FROM Employee
+
+------------------------------------------------------------------
+CREATE PROCEDURE uspGetSalesOrderHeaders
+AS
+BEGIN
+SET NOCOUNT ON
+	SELECT 
+		SalesOrderID,
+		OrderDate,
+		SalesOrderNumber,
+		SalesPersonID
+	from Sales.SalesOrderHeader
+END
+
+CREATE TABLE #TmpWho
+(
+	SalesOrderID INT,
+	OrderDate datetime2,
+	SalesOrderNumber nvarchar(50),
+	SalesPersonID int
+)
+
+INSERT INTO #TmpWho
+exec dbo.uspGetSalesOrderHeaders
+
+select * from #TmpWho
+
+----- Mechanisms to check if all is fine
+ALTER PROCEDURE [HumanResources].[uspUpdateEmployeeHireInfo]
+    @BusinessEntityID [int], 
+    @JobTitle [nvarchar](50), 
+    @HireDate [datetime], 
+    @RateChangeDate [datetime], 
+    @Rate [money], 
+    @PayFrequency [tinyint], 
+    @CurrentFlag [dbo].[Flag] 
+WITH EXECUTE AS CALLER
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    BEGIN TRY
+        BEGIN TRANSACTION;
+
+        UPDATE [HumanResources].[Employee] 
+        SET [JobTitle] = @JobTitle 
+            ,[HireDate] = @HireDate 
+            ,[CurrentFlag] = @CurrentFlag 
+        WHERE [BusinessEntityID] = @BusinessEntityID;
+
+        INSERT INTO [HumanResources].[EmployeePayHistory] 
+            ([BusinessEntityID]
+            ,[RateChangeDate]
+            ,[Rate]
+            ,[PayFrequency]) 
+        VALUES (@BusinessEntityID, @RateChangeDate, @Rate, @PayFrequency);
+
+        COMMIT TRANSACTION;
+    END TRY
+    BEGIN CATCH
+        -- Rollback any active or uncommittable transactions before
+        -- inserting information in the ErrorLog
+        IF @@TRANCOUNT > 0
+        BEGIN
+            ROLLBACK TRANSACTION;
+        END
+
+        EXECUTE [dbo].[uspLogError];
+    END CATCH;
+END;
