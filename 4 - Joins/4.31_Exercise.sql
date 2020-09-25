073fd43bdbf5780640727a7e3c54954a3458d33d
ECODEV\mkoziol <mkoziol@ecovadis.com>
2020-09-25 09:35:37 +0200

    xxxx

---
+++ 4 - Joins/4.31_Exercise.sql
@@ -0,0 +1,6 @@
+﻿/*
+
+Write 2 functions as on the screen.
+[dbo].[ufnRetrieveWorkingDays] - parameters @StartDate, @EndDate 
+dbo.ufnLocalTimeToUtc
+*/
---
+++ 4 - Joins/4.31_Present.sql
@@ -0,0 +1,15 @@
+﻿CREATE OR ALTER FUNCTION [dbo].ufnNumberOfLetterA(@RandomText nvarchar(100))
+   RETURNS INT AS 
+   BEGIN
+	DECLARE @Initial INT = 0;
+	DECLARE @After INT = 0;
+
+	SET @Initial = LEN(@RandomText)
+	SET @After = LEN((REPLACE(@RandomText, 'A', '')))
+
+	RETURN @Initial - @AFTER
+   END
+GO
+
+SELECT [dbo].ufnNumberOfLetterA('AAAafffaa')
+SELECT [dbo].ufnNumberOfLetterA('AAAaaa')
