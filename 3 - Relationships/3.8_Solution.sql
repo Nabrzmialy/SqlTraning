772eacefe7500477316ddd697519b214d8f07a1f
ECODEV\mkoziol <mkoziol@ecovadis.com>
2022-09-02 14:41:48 +0200

    3.8 sol

---
+++ 3 - Relationships/3.8_Solution.sql
@@ -0,0 +1,45 @@
+﻿USE [AdventureWorks2012]
+GO
+
+select * from [3.6].Country
+select * from [3.6].AdultPeople
+
+
+
+
+
+
+CREATE TRIGGER [3.6].[uAdultPeople] ON [3.6].AdultPeople
+AFTER UPDATE AS 
+BEGIN
+    DECLARE @Count int;
+
+    SET @Count = @@ROWCOUNT;
+    IF @Count = 0 
+    BEGIN
+		RETURN;
+	END
+
+	IF NOT UPDATE(IsAlive)
+		RETURN;
+
+	DECLARE @OldValue INT = (SELECT IsAlive FROM deleted)
+	DECLARE @NewValue INT = (SELECT IsAlive FROM inserted)
+
+	IF(@OldValue <> @NewValue)
+	BEGIN
+		IF(@NewValue = 1)
+		BEGIN
+			UPDATE [3.6].Country SET [Population] = [Population] + 1 
+			WHERE CountryId = (SELECT NationalityId FROM inserted)
+		END
+		IF(@NewValue = 0)
+		BEGIN
+			UPDATE [3.6].Country SET [Population] = [Population] - 1 
+			WHERE CountryId = (SELECT NationalityId FROM inserted)
+		END
+	END
+END
+
+
+
