0bcd5b0e6fede5fe3087a2b8c5210fedeaa1d0e6
ECODEV\mkoziol <mkoziol@ecovadis.com>
2022-09-02 15:19:07 +0200

    4.2 sol

---
+++ 4 - Joins/4.2_Solution.sql
@@ -0,0 +1,9 @@
+﻿SELECT
+	 P.LastName AS Nazwisko
+	,P.FirstName AS Imię
+	,E.Gender AS Płeć
+FROM
+	Person.Person P 
+	INNER JOIN HumanResources.Employee E ON P.BusinessEntityID = E.BusinessEntityID
+WHERE
+	E.Gender = 'F' -- F = Female = kobieta 
