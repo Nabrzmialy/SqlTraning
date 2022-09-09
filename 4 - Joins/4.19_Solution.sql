fc7288452372f4f9b21cc65fc0e474d9d8dc614c
ECODEV\mkoziol <mkoziol@ecovadis.com>
2022-09-09 15:07:41 +0200

    alter 4.19

---
+++ 4 - Joins/4.19_Solution.sql
@@ -0,0 +1,26 @@
+﻿BEGIN TRAN
+
+	DECLARE @ControlSum INT = 0
+
+	UPDATE
+	    p
+	SET
+	    p.ListPrice = p.ListPrice + 8.69
+	FROM 
+	    Production.Product p
+		INNER JOIN Production.ProductSubcategory ps on p.ProductSubcategoryID = ps.ProductSubcategoryID
+		INNER JOIN Production.ProductCategory pc on pc.ProductCategoryID = ps.ProductCategoryID
+	WHERE pc.[Name]='Clothing' and p.Color = 'Black'
+
+	SET @ControlSum = @@rowcount
+
+IF(@ControlSum = 16)
+BEGIN
+	COMMIT TRAN
+	PRINT 'COMMIT'
+END
+ELSE
+BEGIN
+	ROLLBACK TRAN
+	PRINT 'ROLLBACK'
+END
