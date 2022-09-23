af3437b032dc17e594642b5a060d06ea79a45b55
ECODEV\mkoziol <mkoziol@ecovadis.com>
2022-09-23 13:48:42 +0200

    4.25 sol

---
+++ 4 - Joins/4.25_Solution.sql
@@ -0,0 +1,34 @@
+﻿DROP VIEW MaterializedView
+
+CREATE VIEW MaterializedView
+WITH SCHEMABINDING
+AS
+(
+	SELECT 
+		soh.SalesOrderID,
+		soh.CustomerID as [Id Klienta],
+		email.EmailAddress as [Email]
+	FROM Sales.SalesOrderHeader soh 
+	INNER JOIN Sales.Customer customer on customer.CustomerID = soh.CustomerID
+	INNER JOIN Person.Person person on customer.PersonID = person.BusinessEntityID
+	INNER JOIN Person.EmailAddress email on person.BusinessEntityID = email.BusinessEntityID
+)
+
+
+CREATE UNIQUE CLUSTERED INDEX 
+    MaterializedView
+ON dbo.MaterializedView(SalesOrderID);
+
+
+SELECT 
+		soh.SalesOrderID,
+		soh.CustomerID as [Id Klienta],
+		email.EmailAddress as [Email]
+	FROM Sales.SalesOrderHeader soh 
+	INNER JOIN Sales.Customer customer on customer.CustomerID = soh.CustomerID
+	INNER JOIN Person.Person person on customer.PersonID = person.BusinessEntityID
+	INNER JOIN Person.EmailAddress email on person.BusinessEntityID = email.BusinessEntityID
+
+SELECT SalesOrderID FROM MaterializedView 
+
+SELECT SalesOrderID FROM MaterializedView WITH (NOEXPAND)
